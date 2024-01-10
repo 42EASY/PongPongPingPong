cc38f48ae992e356395f3df55a5560faf59be2f4
eunbi9n <gombi9_9@naver.com>
2024-01-10 17:43:45 +0900

    fix: swagger 경로 수정

--- backend/transcendence/transcendence/urls.py
+++ backend/transcendence/transcendence/urls.py
@@ -39,7 +39,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(('swagger.urls', 'api'))),
+    path('swagger/', include(('swagger.urls'))),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
