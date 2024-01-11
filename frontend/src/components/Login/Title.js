d80de73b41745893a757c68ebc610698b028d584
im-madylin <im.madylin@gmail.com>
2024-01-11 16:27:01 +0900

    feat: Login 페이지 title css 적용

--- frontend/src/components/Login/Title.js
+++ frontend/src/components/Login/Title.js
@@ -1,19 +1,18 @@
 export default function Title() {
   const $title = document.createElement("div");
-  const $titleText1 = document.createElement("h1");
-  const $titleText2 = document.createElement("h1");
-  const $titleText3 = document.createElement("h1");
-  const $titleText4 = document.createElement("h1");
+  $title.classList.add("title");
+  let letters = "PONG PONG PING PONG".split("");
 
-  $titleText1.innerHTML = "PONG";
-  $titleText2.innerHTML = "PONG";
-  $titleText3.innerHTML = "PING";
-  $titleText4.innerHTML = "PONG";
-
-  $title.appendChild($titleText1);
-  $title.appendChild($titleText2);
-  $title.appendChild($titleText3);
-  $title.appendChild($titleText4);
+  letters.forEach((letter, i) => {
+    const $letter = document.createElement("span");
+    $letter.classList.add("letter");
+    if ((i >= 4 && i <= 8) || (i >= 15 && i <= 19)) {
+      $letter.classList.add("white");
+    }
+    $letter.textContent = letter;
+    $letter.style.animationDelay = `${i / 10}s`;
+    $title.appendChild($letter);
+  });
 
   return $title;
 }
