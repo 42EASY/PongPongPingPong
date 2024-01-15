01005ff1dd1acd480b2433ebbcb7b55614cecd75
eunbi9n <gombi9_9@naver.com>
2024-01-15 22:42:46 +0900

    feat: auth를 위한 urls 추가

--- backend/transcendence/transcendence/urls.py
+++ backend/transcendence/transcendence/urls.py
@@ -44,5 +44,6 @@ urlpatterns = [
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/v1/block/', include(('social.urls'))),
+    path('api/v1/auth/', include(('members.auth_urls'))),
 ]
 
