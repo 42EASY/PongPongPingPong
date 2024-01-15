4d1b5d8d6cbe33a2d946c3cb09d6a509faf7c9b9
dlwndms0812 <dlwndms0812@naver.com>
2024-01-15 16:00:55 +0900

    remove: 사용하지 않는 명령어 삭제

--- clean.sh
+++ clean.sh
@@ -3,4 +3,3 @@ docker rm $(docker ps -q -a)
 docker rmi -f $(docker images -q -a)
 docker volume rm $(docker volume ls -q)
 docker network rm $(docker network ls -q) 2>/dev/null
-sudo rm -rf /home/juelee/data/*
