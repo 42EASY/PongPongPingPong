dfae5c0a5be5b491e16a99ff5af3efbc379ab3d2
Hahyeon Lee <im.madylin@gmail.com>
2024-01-19 22:07:10 +0900

    feat: friends 타이틀 클릭이벤트 추가

--- frontend/src/components/Friends/Title.js
+++ frontend/src/components/Friends/Title.js
@@ -9,7 +9,8 @@ export default function Title() {
   $closeButton.classList.add("closeButton", "bi", "bi-x-lg");
 
   const $friendsList = document.createElement("div");
-  $friendsList.classList.add("titleList");
+  //초기 화면에서는 친구 목록 선택
+  $friendsList.classList.add("titleList", "titleSelect");
   $friendsList.innerHTML = "친구 목록";
 
   const $blockedList = document.createElement("div");
@@ -21,6 +22,18 @@ export default function Title() {
 
   $titleWrapper.appendChild($title);
   $titleWrapper.appendChild($closeButton);
+
+  //친구 목록 클릭 이벤트
+  $friendsList.onclick = () => {
+    $friendsList.classList.add("titleSelect");
+    $blockedList.classList.remove("titleSelect");
+  };
+
+  //차단 목록 클릭 이벤트
+  $blockedList.onclick = () => {
+    $friendsList.classList.remove("titleSelect");
+    $blockedList.classList.add("titleSelect");
+  };
 
   return $titleWrapper;
 }
