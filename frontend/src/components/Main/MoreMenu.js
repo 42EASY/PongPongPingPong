d5b0c30d049382628ac4094dfabab67bdf7d39e1
정성은 <soc06241@naver.com>
2024-01-23 20:56:16 +0900

    feat: 더보기 버튼 클릭 시 드롭다운 추가

--- frontend/src/components/Main/MoreMenu.js
+++ frontend/src/components/Main/MoreMenu.js
@@ -8,14 +8,14 @@ export default function MoreMenu() {
 
   $MoreMenuWrapper.classList.add("moreMenuWrapper");
   $MoreMenuWrapper.appendChild($MoreMenu);
-  $MoreMenu.classList.add("moreMenu");
+  $MoreMenu.classList.add("list-group", "moreMenu");
   $MoreMenu.appendChild($InviteGameOption);
-  $InviteGameOption.classList.add("moreMenuList");
+  $InviteGameOption.classList.add("list-group-item", "moreMenuList");
   $InviteGameOption.appendChild($InviteGameIcon);
   $InviteGameIcon.classList.add("bi", "bi-envelope-open", "menuIcon");
   $InviteGameOption.append("게임초대");
   $MoreMenu.appendChild($BlockOption);
-  $BlockOption.classList.add("moreMenuList", "menuListRed");
+  $BlockOption.classList.add("list-group-item", "moreMenuList", "menuListRed");
   $BlockOption.appendChild($BlockIcon);
   $BlockIcon.classList.add("bi", "bi-person-dash", "menuIcon");
   $BlockOption.append("차단하기");
