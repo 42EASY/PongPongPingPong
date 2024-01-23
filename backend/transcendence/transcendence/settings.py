dcfa55fe50e63468e524ca322994da52c4984b86
eunbi9n <gombi9_9@naver.com>
2024-01-23 13:54:47 +0900

    chore: media_root 및 url 설정

--- backend/transcendence/transcendence/settings.py
+++ backend/transcendence/transcendence/settings.py
@@ -18,6 +18,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'members/static')
+MEDIA_URL = '/transcendence/members/data/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
