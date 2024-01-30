839b29158d86d3ec3d5d49ad3ccf63c69508344f
dlwndms0812 <dlwndms0812@naver.com>
2024-01-30 14:10:46 +0900

    style: 완료한 todo 삭제

--- backend/transcendence/social/block_urls.py
+++ backend/transcendence/social/block_urls.py
@@ -4,7 +4,6 @@ from social.block_views import BlockView
 app_name = 'block';
 
 urlpatterns = [
-    #TODO: base_user_id 제거
     path('/<int:user_id>', BlockView.as_view(), name = 'post'),
     path('/<int:user_id>', BlockView.as_view(), name = 'delete'),
     path('', BlockView.as_view(), name = 'get')
--- backend/transcendence/social/friends_urls.py
+++ backend/transcendence/social/friends_urls.py
@@ -4,7 +4,6 @@ from social.friends_views import FriendsView
 app_name = 'friends'
 
 urlpatterns = [
-    #TODO: base_user_id 제거
     path('/<int:user_id>', FriendsView.as_view(), name = 'post'),
     path('/<int:user_id>', FriendsView.as_view(), name = 'delete'),
     path('', FriendsView.as_view(), name = 'get')
