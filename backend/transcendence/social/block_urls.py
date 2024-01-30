968accda82c2509becbc978c3f5d541385ecc6ef
dlwndms0812 <dlwndms0812@naver.com>
2024-01-30 13:55:36 +0900

    feat: block view에서 토큰 사용하도록 변경 및 테스트 완료

--- backend/transcendence/social/block_urls.py
+++ backend/transcendence/social/block_urls.py
@@ -5,7 +5,7 @@ app_name = 'block';
 
 urlpatterns = [
     #TODO: base_user_id 제거
-    path('/<int:user_id>/<int:base_user_id>', BlockView.as_view(), name = 'post'),
-    path('/<int:user_id>/<int:base_user_id>', BlockView.as_view(), name = 'delete'),
+    path('/<int:user_id>', BlockView.as_view(), name = 'post'),
+    path('/<int:user_id>', BlockView.as_view(), name = 'delete'),
     path('', BlockView.as_view(), name = 'get')
 ]
