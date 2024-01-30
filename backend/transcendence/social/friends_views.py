f64e30e57cb2fc2f4416c31537d04f0437787357
dlwndms0812 <dlwndms0812@naver.com>
2024-01-30 14:09:57 +0900

    feat: friends view에서 토큰 사용하도록 변경 및 테스트 완료

--- backend/transcendence/social/friends_views.py
+++ backend/transcendence/social/friends_views.py
@@ -6,15 +6,16 @@ from members.models import Members
 from social.models import Friend
 from social.models import Block
 from django.core.paginator import Paginator
+from security.views import login_required
 
 class FriendsView(APIView):
     permissions_classes = [permissions.AllowAny]
 
-    #TODO: base_user_id 사용 대신 토큰 사용으로 변경
-    def post(self, request, user_id, base_user_id):
+    @login_required
+    def post(self, request, user_id):
         try:
             target_user = Members.objects.get(id = user_id)
-            base_user = Members.objects.get(id = base_user_id)
+            base_user = request.user
 
             if (Block.objects.filter(user = base_user, target = target_user).count() > 0):
                 return JsonResponse({
@@ -43,11 +44,11 @@ class FriendsView(APIView):
         }, status = 201)
     
 
-    def delete(self, request, user_id, base_user_id):
-        #TODO: base_user_id 사용 대신 토큰 사용으로 변경
+    @login_required
+    def delete(self, request, user_id):
         try:
             target_user = Members.objects.get(id = user_id)
-            base_uer = Members.objects.get(id = base_user_id)
+            base_uer = request.user
 
         except:
             return JsonResponse({
@@ -75,14 +76,13 @@ class FriendsView(APIView):
         }, status = 200)
     
 
+    @login_required
     def get(self, request):
         keyword = request.GET.get('keyword', None)
         page = request.GET.get('page', None)
         size = request.GET.get('size', None)
-        #TODO: 토큰이 적용되면, query string대신 token으로 user_id 받기 
-        user_id = request.GET.get('user_id', None)
-
-        if (page == None or page == '' or user_id == None or user_id == ''):
+        
+        if (page == None or page == ''):
             return JsonResponse({
 				'code': 400,
 				'message':'NULL Error'
@@ -95,7 +95,7 @@ class FriendsView(APIView):
         data = []
        
         try:
-            base_user = Members.objects.get(id = user_id)
+            base_user = request.user
 
             #keyword가 비어있는 경우 전체 리스트 반환
             if (keyword == None or keyword == ''):
--- backend/transcendence/social/test_friends_views.py
+++ backend/transcendence/social/test_friends_views.py
@@ -1,17 +1,22 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from members.models import Members
 from social.models import Friend
 from social.models import Block
 from django.urls import reverse
 from urllib.parse import urlencode
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.test import APIClient
 
-client = Client()
+client = APIClient()
 
 class FriendsViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user_model = Members.objects.create(nickname = 'base_user', email = 'user@test.com', is_2fa = False,
-                                      image_url = 'test_url')
+        cls.fake_user = Members.objects.create(nickname='loginUser', email='loginUser@email.com', is_2fa=False)
+        refresh = RefreshToken.for_user(cls.fake_user)
+        fake_token = str(refresh.access_token)
+        
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + fake_token)
         
         cls.target_model = Members.objects.create(nickname = 'target', email = 'target@test.com', is_2fa = False,
                                         image_url = 'test_url')
@@ -20,16 +25,14 @@ class FriendsViewTest(TestCase):
             dummy_nickname = 'dummy' + str(i)
             dummy_email = 'dummy' + str(i) + '@test.com'
             dummy = Members.objects.create(nickname = dummy_nickname, email = dummy_email, is_2fa = False, image_url = 'test_url')
-            Friend.objects.create(user = cls.user_model, target = dummy)
+            Friend.objects.create(user = cls.fake_user, target = dummy)
 
 
-    #base_user가 target을 친구 추가 성공 테스트
-    #TODO: 토큰 미적용하여 /api/v1/friends/{user-id}/{base-user-id}로 사용, 추후 /api/v1/friends/{user-id}로 변경 예정
+    #loginuser가 target을 친구 추가 성공 테스트
     def test_post_friends_success(self):
-        user_model = Members.objects.get(nickname = 'base_user')
         target_model = Members.objects.get(nickname = 'target')
 
-        url = reverse('friends:post', kwargs = {'user_id' : target_model.id, 'base_user_id' : user_model.id })
+        url = reverse('friends:post', kwargs = {'user_id' : target_model.id})
         response = client.post(url)
 
         self.assertEquals(response.json()['code'], 201)
@@ -37,67 +40,53 @@ class FriendsViewTest(TestCase):
 
 
     #target user가 없는 유저 친구 추가 실패 테스트    
-    #TODO: 토큰 미적용하여 /api/v1/friends/{user-id}/{base-user-id}로 사용, 추후 /api/v1/friends/{user-id}로 변경 예정
     def test_post_friends_no_target_user(self):
-        user_model = Members.objects.get(nickname = 'base_user')
-
-        url = reverse('friends:post', kwargs = {'user_id' : 100, 'base_user_id' : user_model.id})
+        url = reverse('friends:post', kwargs = {'user_id' : 100})
         response = client.post(url)
 
         self.assertEquals(response.json()['code'], 404)
         self.assertEquals(response.status_code, 404)
 
 
-    #base_user가 target를 친구 차단 했을 때의 친구 추가 실패 테스트 
-    #TODO: 토큰 미적용하여 /api/v1/friends/{user-id}/{base-user-id}로 사용, 추후 /api/v1/friends/{user-id}로 변경 예정
+    #loginuser가 target를 친구 차단 했을 때의 친구 추가 실패 테스트 
     def test_post_friends_block_friend(self):
-        user_model = Members.objects.get(nickname = 'base_user')
         target_model = Members.objects.get(nickname = 'target')
 
-        Block.objects.create(user = user_model, target = target_model)
+        Block.objects.create(user = self.fake_user, target = target_model)
 
-        url = reverse('friends:post', kwargs = {'user_id' : target_model.id, 'base_user_id' : user_model.id })
+        url = reverse('friends:post', kwargs = {'user_id' : target_model.id})
         response = client.post(url)
 
         self.assertEquals(response.json()['code'], 409)
         self.assertEquals(response.status_code, 409)
 
-    #base_user가 친구 추가된 target을 친구 삭제 성공테스트
-    #TODO: 토큰 미적용하여 /api/v1/friends/{user-id}/{base-user-id}로 사용, 추후 /api/v1/friends/{user-id}로 변경 예정
+    #loginuser가 친구 추가된 target을 친구 삭제 성공테스트
     def test_delete_friends_success(self):
-        user_model = Members.objects.get(nickname = 'base_user')
         target_model = Members.objects.get(nickname = 'target')
 
-        Friend.objects.create(user = user_model, target = target_model)
+        Friend.objects.create(user = self.fake_user, target = target_model)
 
-        url = reverse('friends:delete', kwargs = {'user_id' : target_model.id, 'base_user_id' : user_model.id})
+        url = reverse('friends:delete', kwargs = {'user_id' : target_model.id})
         response = client.delete(url)
 
         self.assertEquals(response.json()['code'], 200)
         self.assertEquals(response.status_code, 200)
 
     #target 유저가 없는 친구 삭제 실패 테스트
-    #TODO: 토큰 미적용하여 /api/v1/friends/{user-id}/{base-user-id}로 사용, 추후 /api/v1/friends/{user-id}로 변경 예정
     def test_delete_friends_no_user(self):
-        user_model = Members.objects.get(nickname = 'base_user')
-
-        url = reverse('friends:delete', kwargs = {'user_id' : 100, 'base_user_id' : user_model.id})
+        url = reverse('friends:delete', kwargs = {'user_id' : 100})
         response = client.delete(url)
 
         self.assertEquals(response.json()['code'], 404)
         self.assertEquals(response.status_code, 404)
     
 
-    #user의 친구 목록 반환 테스트(keyword x)
-    #TODO: /api/v1/friends?keyword={keyword}&page={page}&size={size}&user_id={user_id} 에서 user_id 삭제 예정
+    #loginuser의 친구 목록 반환 테스트(keyword x)
     def test_get_friends_list_no_keyword(self):
-        user_model = Members.objects.get(nickname = 'base_user')
-
         query_params = {
            'keyword': '',
            'page': 1,
            'size': 5,
-           'user_id': user_model.id,
         }
 
         query_string = urlencode(query_params)
@@ -110,19 +99,15 @@ class FriendsViewTest(TestCase):
 
 
 
-    #user의 친구 목록 반환 테스트(keyword o)
-    #TODO: /api/v1/friends?keyword={keyword}&page={page}&size={size}&user_id={user_id} 에서 user_id 삭제 예정
+    #loginuser의 친구 목록 반환 테스트(keyword o)
     def test_get_friends_list_keyword(self):
-        user_model = Members.objects.get(nickname = 'base_user')
-
         tmp = Members.objects.create(nickname = 'tmp', email = 'tmp@test.com', is_2fa = False, image_url = 'test_url')
-        Friend.objects.create(user = user_model, target = tmp)
+        Friend.objects.create(user = self.fake_user, target = tmp)
 
         query_params = {
            'keyword': 't',
            'page': 1,
-           'size': 1,
-           'user_id': user_model.id,
+           'size': 1
         }
 
         query_string = urlencode(query_params)
@@ -135,16 +120,12 @@ class FriendsViewTest(TestCase):
         self.assertEquals(response.json()['result']['total_page'], 1)
         
 
-    #user의 친구 목록 반환 실패(유효하지 않는 size - 음수일 때)
-    #TODO: /api/v1/friends?keyword={keyword}&page={page}&size={size}&user_id={user_id} 에서 user_id 삭제 예정
+    #loginuser의 친구 목록 반환 실패(유효하지 않는 size - 음수일 때)
     def test_get_friends_list_invalid_size(self):
-        user_model = Members.objects.get(nickname = 'base_user')
-
         query_params = {
            'keyword': '',
            'page': 1,
            'size': -1,
-           'user_id': user_model.id,
         }
 
         query_string = urlencode(query_params)
@@ -155,16 +136,12 @@ class FriendsViewTest(TestCase):
         self.assertEquals(response.json()['code'], 400)
         self.assertEquals(response.status_code, 400)
 
-    #user의 친구 목록 반환 실패(유효하지 않는 size - 0일때)
-    #TODO: /api/v1/friends?keyword={keyword}&page={page}&size={size}&user_id={user_id} 에서 user_id 삭제 예정
+    #loginuser의 친구 목록 반환 실패(유효하지 않는 size - 0일때)
     def test_get_friends_list_invalid_size_zero(self):
-        user_model = Members.objects.get(nickname = 'base_user')
-
         query_params = {
            'keyword': '',
            'page': 1,
            'size': 0,
-           'user_id': user_model.id,
         }
 
         query_string = urlencode(query_params)
@@ -176,22 +153,18 @@ class FriendsViewTest(TestCase):
         self.assertEquals(response.status_code, 400)
 
 
-    #user의 친구 목록 반환 성공(size가 빈칸일 때)
-    #TODO: /api/v1/friends?keyword={keyword}&page={page}&size={size}&user_id={user_id} 에서 user_id 삭제 예정
+    #loginuser의 친구 목록 반환 성공(size가 빈칸일 때)
     def test_get_friends_list_invalid_size_null(self):
-        user_model = Members.objects.get(nickname = 'base_user')
-
         for i in range(5):
             dummy_nickname = 'aa' + str(i)
             dummy_email = 'aa' + str(i) + '@test.com'
             dummy = Members.objects.create(nickname = dummy_nickname, email = dummy_email, is_2fa = False, image_url = 'test_url')
-            Friend.objects.create(user = user_model, target = dummy)
+            Friend.objects.create(user = self.fake_user, target = dummy)
 
         query_params = {
            'keyword': 'aa',
            'page': 1,
-           'size': '',
-           'user_id': user_model.id,
+           'size': ''
         }
 
         query_string = urlencode(query_params)
@@ -203,16 +176,12 @@ class FriendsViewTest(TestCase):
         self.assertEquals(response.status_code, 200)
     
 
-    #user의 친구 목록 반환 실패(유효하지 않는 페이지 - 음수일 때)
-    #TODO: /api/v1/friends?keyword={keyword}&page={page}&size={size}&user_id={user_id} 에서 user_id 삭제 예정
+    #loginuser의 친구 목록 반환 실패(유효하지 않는 페이지 - 음수일 때)
     def test_get_friends_list_invalid_page_negative(self):
-        user_model = Members.objects.get(nickname = 'base_user')
-
         query_params = {
            'keyword': '',
            'page': -1,
-           'size': 10,
-           'user_id': user_model.id,
+           'size': 10
         }
 
         query_string = urlencode(query_params)
@@ -224,16 +193,12 @@ class FriendsViewTest(TestCase):
         self.assertEquals(response.status_code, 400)
 
 
-    #user의 친구 목록 반환 실패(유효하지 않는 페이지 - 0일때)
-    #TODO: /api/v1/friends?keyword={keyword}&page={page}&size={size}&user_id={user_id} 에서 user_id 삭제 예정
-    def test_get_friends_list_invalid_page_zero(self):
-        user_model = Members.objects.get(nickname = 'base_user')
-       
+    #loginuser의 친구 목록 반환 실패(유효하지 않는 페이지 - 0일때)
+    def test_get_friends_list_invalid_page_zero(self):  
         query_params = {
            'keyword': '',
            'page': 0,
-           'size': 10,
-           'user_id': user_model.id,
+           'size': 10
         }
 
         query_string = urlencode(query_params)
@@ -245,16 +210,12 @@ class FriendsViewTest(TestCase):
         self.assertEquals(response.status_code, 400)
 
 
-    #user의 친구 목록 반환 실패(유효하지 않는 페이지 - 빈칸일 때)
-    #TODO: /api/v1/friends?keyword={keyword}&page={page}&size={size}&user_id={user_id} 에서 user_id 삭제 예정
+    #loginuser의 친구 목록 반환 실패(유효하지 않는 페이지 - 빈칸일 때)
     def test_get_friends_list_invalid_page_blank(self):
-        user_model = Members.objects.get(nickname = 'base_user')
-
         query_params = {
            'keyword': '',
            'page': '',
            'size': 10,
-           'user_id': user_model.id,
         }
 
         query_string = urlencode(query_params)
@@ -266,16 +227,12 @@ class FriendsViewTest(TestCase):
         self.assertEquals(response.status_code, 400)
 
     
-    #user의 차단 목록 반환 실패(유효하지 않는 페이지 - 존재하는 페이지보다 큰 수 일때)
-    #TODO: /api/v1/friends?keyword={keyword}&page={page}&size={size}&user_id={user_id} 에서 user_id 삭제 예정
+    #loginuser의 차단 목록 반환 실패(유효하지 않는 페이지 - 존재하는 페이지보다 큰 수 일때)
     def test_get_friends_list_invalid_page_big(self):
-        user_model = Members.objects.get(nickname = 'base_user')
-
         query_params = {
            'keyword': '',
            'page': 10000,
-           'size': 10,
-           'user_id': user_model.id,
+           'size': 10
         }
 
         query_string = urlencode(query_params)
