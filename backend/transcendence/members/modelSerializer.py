0c8a5bcb24ed567b9d938b6785a1e24b5717c092
eunbi9n <gombi9_9@naver.com>
2024-02-03 00:48:38 +0900

    fix: 닉네임 중복 검사 추가

--- backend/transcendence/members/modelSerializer.py
+++ backend/transcendence/members/modelSerializer.py
@@ -5,3 +5,10 @@ class MemberSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Members
 		fields = ('nickname', 'is_2fa')
+
+	def validate_nickname(self, value):
+		# 닉네임 필드에 대한 중복 체크를 수행하는 사용자 정의 유효성 검사 함수
+		# Members 모델에서 닉네임이 이미 존재하는지 확인하고, 존재한다면 ValidationError을 발생시킵니다.
+		if Members.objects.filter(nickname=value).exists():
+			raise serializers.ValidationError("Duplicate nicknames")
+		return value
