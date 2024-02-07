dc9999b0a23a907a7f6033efb6829b0a56c6eb49
정성은 <soc06241@naver.com>
2024-02-07 14:29:22 +0900

    feat: nav api 추가

---
+++ frontend/src/components/Nav/NavApi.js
@@ -0,0 +1,37 @@
+import { getAccessToken } from "../../state/State.js";
+
+const baseUrl = "http://localhost:8000";
+
+export async function getUserList(keyword, size) {
+  try {
+    const url =
+      baseUrl + `/api/v1/members/search?keyword=${keyword}&page=1&size=${size}`;
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    });
+    const json = await res.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function postLogout() {
+  try {
+    const url = baseUrl + `/api/v1/auth/logout`;
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    });
+    await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
