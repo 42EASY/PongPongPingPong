41c1893b30bb1137a1e22d724bb9a6f95a19cff7
정성은 <soc06241@naver.com>
2024-02-08 13:33:40 +0900

    fix: code 401일 경우 처리 추가

--- frontend/src/components/Main/UserApi.js
+++ frontend/src/components/Main/UserApi.js
@@ -1,84 +1,84 @@
-import { getAccessToken } from "../../state/State.js";
+import { getAccessToken, setNewAccessToken } from "../../state/State.js";
 
 const baseUrl = "http://localhost:8000";
 
 export async function getUserInfo(id) {
-  try {
-    const url = baseUrl + `/api/v1/members/${id}`;
-    const res = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${getAccessToken()}`,
-      },
-    });
-    const json = await res.json();
-    return json;
-  } catch (error) {
-    console.log(error);
+  const url = baseUrl + `/api/v1/members/${id}`;
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  });
+  const json = await res.json();
+  if (json.code === 401) {
+    setNewAccessToken();
+    getUserInfo(id);
   }
+  return json;
 }
 
 export async function postFriend(id) {
-  try {
-    const url = baseUrl + `/api/v1/friends/${id}`;
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${getAccessToken()}`,
-      },
-    });
-    await res.json();
-  } catch (error) {
-    console.log(error);
+  const url = baseUrl + `/api/v1/friends/${id}`;
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  });
+  const json = await res.json();
+  if (json.code === 401) {
+    setNewAccessToken();
+    postFriend(id);
   }
 }
 
 export async function deleteFriend(id) {
-  try {
-    const url = baseUrl + `/api/v1/friends/${id}`;
-    const res = await fetch(url, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${getAccessToken()}`,
-      },
-    });
-    await res.json();
-  } catch (error) {
-    console.log(error);
+  const url = baseUrl + `/api/v1/friends/${id}`;
+  const res = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  });
+  const json = await res.json();
+  if (json.code === 401) {
+    setNewAccessToken();
+    deleteFriend(id);
   }
 }
 
 export async function postBlock(id) {
-  try {
-    const url = baseUrl + `/api/v1/block/${id}`;
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${getAccessToken()}`,
-      },
-    });
-    await res.json();
-  } catch (error) {
-    console.log(error);
+  const url = baseUrl + `/api/v1/block/${id}`;
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  });
+  const json = await res.json();
+  if (json.code === 401) {
+    setNewAccessToken();
+    postBlock(id);
   }
 }
 
 export async function deleteBlock(id) {
-  try {
-    const url = baseUrl + `/api/v1/block/${id}`;
-    const res = await fetch(url, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${getAccessToken()}`,
-      },
-    });
-    await res.json();
-  } catch (error) {
-    console.log(error);
+  const url = baseUrl + `/api/v1/block/${id}`;
+  const res = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  });
+  const json = await res.json();
+  if (json.code === 401) {
+    setNewAccessToken();
+    deleteBlock(id);
   }
 }
--- frontend/src/components/Nav/NavApi.js
+++ frontend/src/components/Nav/NavApi.js
@@ -1,38 +1,38 @@
-import { getAccessToken } from "../../state/State.js";
+import { getAccessToken, setNewAccessToken } from "../../state/State.js";
 
 const baseUrl = "http://localhost:8000";
 
 export async function getUserList(keyword, page, size) {
-  try {
-    const url =
-      baseUrl +
-      `/api/v1/members/search?keyword=${keyword}&page=${page}&size=${size}`;
-    const res = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${getAccessToken()}`,
-      },
-    });
-    const json = await res.json();
-    return json;
-  } catch (error) {
-    console.log(error);
+  const url =
+    baseUrl +
+    `/api/v1/members/search?keyword=${keyword}&page=${page}&size=${size}`;
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  });
+  const json = await res.json();
+  if (json.code === 401) {
+    setNewAccessToken();
+    getUserList(keyword, page, size);
   }
+  return json;
 }
 
 export async function postLogout() {
-  try {
-    const url = baseUrl + `/api/v1/auth/logout`;
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${getAccessToken()}`,
-      },
-    });
-    await res.json();
-  } catch (error) {
-    console.log(error);
+  const url = baseUrl + `/api/v1/auth/logout`;
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  });
+  const json = await res.json();
+  if (json.code === 401) {
+    setNewAccessToken();
+    postLogout();
   }
 }
