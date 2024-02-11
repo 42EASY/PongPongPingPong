eea326ba75fd4296e35b22c11a5162557da1c8f0
dlwndms0812 <dlwndms0812@naver.com>
2024-02-11 16:31:35 +0900

    fix: websocket을 동기식에서 비동기식으로 수정 및 테스트

--- backend/transcendence/games/consumers.py
+++ backend/transcendence/games/consumers.py
@@ -1,44 +1,42 @@
 import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+# from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 #TODO: 연결되는 것 확인하기 위해 임시로 코드 작성
-class GameConsumer(WebsocketConsumer):
-    def connect(self):
+class GameConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
         print('hi')
         self.game_id = 10
         self.channel_name = 'hi'
         self.game_group_id = f"game_{self.game_id}"
 
         # Join group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.game_group_id, self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.game_group_id, self.channel_name
         )
 
-    def receive(self, text_data):
+    #connect 이후 receive 및 send 할 내용 정의
+    async def receive(self, text_data):
         
         print('receive')
-        # Echo message back to the group
-        async_to_sync(self.channel_layer.group_send)(
-            self.game_group_id,
-            {
+
+        await self.send_json({
                 'type': 'game_message',
-                #'message': text_data_json['message']  # Echo back received message
                 'message': 'hello'
-            }
+            }    
         )
 
     # Method to receive message from group
-    def game_message(self, event):
+    async def game_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
