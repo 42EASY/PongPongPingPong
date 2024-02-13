0949acf8022b46f3b737526d77a7a55059f2bef0
eunbi9n <gombi9_9@naver.com>
2024-02-13 10:43:21 +0900

    feat: 임시 라우트 추가

--- backend/transcendence/transcendence/routing.py
+++ backend/transcendence/transcendence/routing.py
@@ -1,7 +1,9 @@
 from django.urls import re_path, path
 from games.consumers import GameConsumer
+from chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
     #path(r'ws/game/<int:room_id>', GameConsumer.as_asgi()),
     path(r'ws/game/<int:room_id>', GameConsumer.as_asgi()),
+    re_path(r'ws/chat', ChatConsumer.as_asgi()), # 임시 라우트
 ]
