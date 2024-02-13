8c240ff1834a3d83a91ac77d3e0baa28ff1d6a6b
Hahyeon Lee <im.madylin@gmail.com>
2024-02-13 16:41:28 +0900

    fix: 2차인증 로직 제대로 적용

--- frontend/src/components/Register/TwoFactorAuth.js
+++ frontend/src/components/Register/TwoFactorAuth.js
@@ -1,4 +1,8 @@
-import { setNewAccessToken, getAccessToken } from "../../state/State.js";
+import {
+  setNewAccessToken,
+  getAccessToken,
+  setIs2fa,
+} from "../../state/State.js";
 import Modal from "../Modal/Modal.js";
 
 export default function TwoFactorAuth(is2fa) {
@@ -53,84 +57,114 @@ export default function TwoFactorAuth(is2fa) {
   $twoFactorAuthWrapper.appendChild($twoFactorAuthText);
   $twoFactorAuthWrapper.appendChild($twoFactorAuthContentWrapper);
 
-  //2차 인증 QR코드 API 호출
-  function call2faQrApi() {
-    const url = "http://localhost:8000/api/v1/auth/2fa/";
-
-    fetch(url, {
-      method: "GET",
-      headers: {
-        "content-Type": "application/json",
-        Authorization: `Bearer ${getAccessToken()}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.code === 200) {
-          Modal("tfa");
-          const $modalBodyQr = document.querySelector(".modalBody img");
-          $modalBodyQr.setAttribute(
-            "src",
-            `data:image/png;base64,${data.result.encoded_image}`
-          );
-        } else if (data.code === 401) {
-          setNewAccessToken();
-          call2faQrApi();
-        }
-      });
-  }
-
-  //2차 인증 OTP API 호출
-  function call2faOtqApi() {
-    //모달 리턴값으로 otp 코드 받기
-    const url = "http://localhost:8000/api/v1/auth/2fa/";
-
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "content-Type": "application/json",
-        Authorization: `Bearer ${getAccessToken()}`,
-      },
-      body: JSON.stringify({
-        otp: "test",
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.code === 200) {
-          //인증 성공 모달 띄우기
-        } else if (data.code === 401) {
-          setNewAccessToken();
-          call2faOtqApi();
-        } else {
-          //인증 실패 모달 띄우기
-        }
-      });
-  }
-
   //비활성화 클릭 이벤트
   $twoFactorAuthDeactive.addEventListener("click", () => {
+    if ($twoFactorAuthDeactive.classList.contains("twoFactorAuthSelect"))
+      return;
     $twoFactorAuthActive.classList.remove("twoFactorAuthSelect");
     $twoFactorAuthDeactive.classList.add("twoFactorAuthSelect");
-    if (is2fa === false) return;
-    //is2fa가 true인 경우, otp 인증
-    Modal("otp");
+    console.log(is2fa);
+
+    if (is2fa === "true") {
+      //is2fa가 true인 경우, otp 인증
+      Modal("otp").then(async (result) => {
+        if (result.isPositive === true) {
+          const status = await call2faOtqApi(result.input);
+          console.log(status);
+          if (status === true) {
+            console.log("2차 인증 성공");
+            setIs2fa(false);
+            return;
+          } else if (status === false) {
+            console.log("2차 인증 실패");
+          }
+        }
+        $twoFactorAuthActive.classList.add("twoFactorAuthSelect");
+        $twoFactorAuthDeactive.classList.remove("twoFactorAuthSelect");
+      });
+    }
     //todo: 모달 결과에 따라 select 변경
   });
 
   //활성화 클릭 이벤트
   $twoFactorAuthActive.addEventListener("click", () => {
+    if ($twoFactorAuthActive.classList.contains("twoFactorAuthSelect")) return;
     $twoFactorAuthActive.classList.add("twoFactorAuthSelect");
     $twoFactorAuthDeactive.classList.remove("twoFactorAuthSelect");
 
     call2faQrApi();
-    Modal("otp");
-    const $modalOtpSummit = document.querySelector(".otpSummit");
-    $modalOtpSummit.addEventListener("click", call2faOtqApi);
-    //todo: 모달 결과에 따라 select 변경
+    Modal("otp").then(async (result) => {
+      if (result.isPositive === true) {
+        const status = await call2faOtqApi(result.input);
+        console.log(status);
+        if (status === true) {
+          console.log("2차 인증 성공");
+          setIs2fa(true);
+          return;
+        } else if (status === false) {
+          console.log("2차 인증 실패");
+        }
+      }
+      $twoFactorAuthActive.classList.remove("twoFactorAuthSelect");
+      $twoFactorAuthDeactive.classList.add("twoFactorAuthSelect");
+    });
   });
 
   return $twoFactorAuthWrapper;
+}
+
+//2차 인증 QR코드 API 호출
+function call2faQrApi() {
+  const url = "http://localhost:8000/api/v1/auth/2fa/";
+
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "content-Type": "application/json",
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.code === 200) {
+        Modal("tfa");
+        const $modalBodyQr = document.querySelector(".modalBody img");
+        $modalBodyQr.setAttribute(
+          "src",
+          `data:image/png;base64,${data.result.encoded_image}`
+        );
+      } else if (data.code === 401) {
+        setNewAccessToken();
+        call2faQrApi();
+      }
+    });
+}
+
+//2차 인증 OTP API 호출
+async function call2faOtqApi(otpNum) {
+  const url = "http://localhost:8000/api/v1/auth/2fa/";
+
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "content-Type": "application/json",
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+    body: JSON.stringify({
+      otp_code: otpNum,
+    }),
+  });
+  const data = await res.json();
+  console.log(data);
+  if (data.code === 200) {
+    //인증 성공 모달 띄우기
+    return true;
+  } else if (data.code === 401 && data.message === "2차 인증 실패") {
+    //인증 실패 모달 띄우기
+    return false;
+  } else if (data.code === 401) {
+    setNewAccessToken();
+    call2faOtqApi();
+  }
 }
