c268a65451ec6901ad326024ada7a6d372cbb25e
eunbi9n <gombi9_9@naver.com>
2024-02-15 02:46:50 +0900

    fix: 소켓 라우트 분리

--- backend/transcendence/transcendence/routing.py
+++ backend/transcendence/transcendence/routing.py
@@ -1,9 +1,10 @@
 from django.urls import re_path, path
 from games.consumers import GameConsumer
-from chat.consumers import ChatConsumer
+from chat import consumers
 
 websocket_urlpatterns = [
     #path(r'ws/game/<int:room_id>', GameConsumer.as_asgi()),
     path(r'ws/game/<int:room_id>', GameConsumer.as_asgi()),
-    re_path(r'ws/chat', ChatConsumer.as_asgi()), # 임시 라우트
+    re_path(r'ws/join/$', consumers.BaseConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<target_user_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
