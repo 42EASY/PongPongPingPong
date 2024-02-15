0a11e81d13b302e29d2c1bb65987c916973af1da
dlwndms0812 <dlwndms0812@naver.com>
2024-02-15 14:23:49 +0900

    test: 토너먼트에서 빠른시작을 할 경우 테스트코드 작성

--- backend/transcendence/games/test_game_queue_consumer.py
+++ backend/transcendence/games/test_game_queue_consumer.py
@@ -708,3 +708,45 @@ async def test_invite_tournament_success():
 
     await communicator.disconnect()
          
+
+#-------------------------------------------------------------------------------
+
+#토너먼트 빠른 시작인 경우 테스트코드
+@pytest.mark.asyncio
+async def test_join_tournament_success():
+
+    channel_layer = get_channel_layer()
+
+    # 테스트용 토큰 발급(비동기적으로 실행되기에 테스트용 데이터를 pytest.fixture로 하나로 묶을 수 없음)
+    fake_user = Members.objects.create(nickname = 't11111', email = 'testUser@test.com', is_2fa = False)
+    refresh = RefreshToken.for_user(fake_user)
+    fake_token = str(refresh.access_token)
+
+    current_time = datetime(2024, 2, 13, 12, 0, 0)
+    
+    current_time2 = datetime(2024, 2, 13, 12, 0, 42)
+
+    iso_8601_current_time = current_time.isoformat()
+    iso_8601_current_time2 = current_time2.isoformat()
+
+    test_user = Members.objects.create(nickname = '11111', email = 'tt@test.com', is_2fa = False)
+    
+    #토큰과 함께 ws/join_queue 에 연결
+    communicator = WebsocketCommunicator(GameQueueConsumer.as_asgi(), "/ws/join_queue?token=" + fake_token)
+    connected, subprotocol = await communicator.connect()
+
+    assert connected
+
+    await communicator.send_json_to({
+        "action": "join_tournament_queue",
+        "user_id": test_user.id,
+        "current_time": iso_8601_current_time2
+    })
+
+
+    response = await communicator.receive_json_from()    
+
+    assert response["status"] == "success"
+    
+    await communicator.disconnect()
+
