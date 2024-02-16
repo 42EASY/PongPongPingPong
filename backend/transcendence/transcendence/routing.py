150d4795f4bd24bd46dd5878be91ea08ca91a365
dlwndms0812 <dlwndms0812@naver.com>
2024-02-16 16:06:19 +0900

    fix: path 대신 re_path 사용

--- backend/transcendence/transcendence/routing.py
+++ backend/transcendence/transcendence/routing.py
@@ -4,8 +4,8 @@ from games.game_queue_consumer import GameQueueConsumer
 from chat import consumers
 
 websocket_urlpatterns = [
-    path(r'ws/game/<int:room_id>', GameConsumer.as_asgi()),
-    path(r'ws/join_queue', GameQueueConsumer.as_asgi()),
+    path(r'ws/game/<int:room_id>', GameConsumer.as_asgi()), #TODO: 테스트용이므로 지울 예정
+    re_path(r'ws/join_queue', GameQueueConsumer.as_asgi()),
     re_path(r'ws/join/$', consumers.BaseConsumer.as_asgi()),
     re_path(r'ws/chat/(?P<target_user_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
