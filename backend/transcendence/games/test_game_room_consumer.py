d3b017823f9f3283baeb78f6d2625d70fd92373e
dlwndms0812 <dlwndms0812@naver.com>
2024-02-17 20:04:00 +0900

    test: 결승 시작 대기 및 게임 시작 response 성공 테스트코드 작성

--- backend/transcendence/games/test_game_room_consumer.py
+++ backend/transcendence/games/test_game_room_consumer.py
@@ -43,7 +43,8 @@ async def test_join_room_success():
             { "user_id" : dummy_user3.id, "channel_id": "123"},
             { "user_id" : fake_user.id, "channel_id": "123"}],
         "invited_info": [],
-        "join_user": [ dummy_user1.id, dummy_user2.id, dummy_user3.id ]
+        "join_user": [ dummy_user1.id, dummy_user2.id, dummy_user3.id ],
+        "join_final_user": []
     }
 
     cache.set('tournament_' + str(tournament.id), json.dumps(value))
@@ -104,7 +105,8 @@ async def test_invite_and_join_room_success():
             { "user_id" : dummy_user2.id, "channel_id": "123"},
             { "user_id" : fake_user.id, "channel_id": "123"}],
         "invited_info": [],
-        "join_user": [ dummy_user1.id, dummy_user2.id ]
+        "join_user": [ dummy_user1.id, dummy_user2.id ],
+        "join_final_user": []
     }
 
     cache.set('tournament_' + str(tournament.id), json.dumps(value))
@@ -168,3 +170,72 @@ async def test_invite_and_join_room_success():
 
     await invite_communicator.disconnect()
     await join_communicator.disconnect()
+
+
+#-------------------------------------------------------------------------------
+
+#게임방 2명 입장(결승) 성공 테스트
+@pytest.mark.asyncio
+async def test_join_final_room_success():
+
+    channel_layer = get_channel_layer()
+
+    # 테스트용 토큰 발급(비동기적으로 실행되기에 테스트용 데이터를 pytest.fixture로 하나로 묶을 수 없음)
+    fake_user = Members.objects.create(nickname = 'test', email = 'testUser@test.com', is_2fa = False)
+    refresh = RefreshToken.for_user(fake_user)
+    fake_token = str(refresh.access_token)
+
+
+    #더미 데이터 입력 
+    dummy_user1 = Members.objects.create(nickname = 'dummy1', email = 'dummy@test.com', is_2fa = False)
+    dummy_user2 = Members.objects.create(nickname = 'dummy2', email = 'dummy@test.com', is_2fa = False)
+    
+    another_user = Members.objects.create(nickname = 'another', email = 'another@test.com', is_2fa = False)
+    another_refresh = RefreshToken.for_user(another_user)
+    another_token = str(another_refresh.access_token)
+    
+    tournament = Tournament.objects.create()
+
+    value = {
+        "registered_user": [
+            { "user_id" : dummy_user1.id, "channel_id": "123"},
+            { "user_id" : dummy_user2.id, "channel_id": "123"},
+            { "user_id" : another_user.id, "channel_id": "123"},
+            { "user_id" : fake_user.id, "channel_id": "123"}],
+        "invited_info": [],
+        "join_user": [ dummy_user1.id, dummy_user2.id, another_user.id, fake_user.id ],
+        "join_final_user": []
+    }
+
+    cache.set('tournament_' + str(tournament.id), json.dumps(value))
+
+    #another_user가 발급 받은 게임방 아이디로 접속
+    another_communicator = WebsocketCommunicator(GameRoomConsumer.as_asgi(), "/ws/join_room/" + str(tournament.id) +"?token=" + another_token)
+    another_connected, another_subprotocol = await another_communicator.connect()
+
+    assert another_connected
+
+    await another_communicator.send_json_to({
+        "action": "join_final",
+        "user_id": another_user.id
+    })
+
+    #fake_user가 발급 받은 게임방 아이디로 접속
+    communicator = WebsocketCommunicator(GameRoomConsumer.as_asgi(), "/ws/join_room/" + str(tournament.id) +"?token=" + fake_token)
+    connected, subprotocol = await communicator.connect()
+
+    assert connected
+
+    await communicator.send_json_to({
+        "action": "join_final",
+        "user_id": fake_user.id
+    })
+
+    response = await communicator.receive_json_from()    
+
+    assert response["status"] == "game_start_soon"
+
+    await communicator.disconnect()
+
+
+#-------------------------------------------------------------------------------
