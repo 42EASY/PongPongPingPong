51f846882738c9bd21f8f8c0acc60d8482e7bfd2
dlwndms0812 <dlwndms0812@naver.com>
2024-02-19 16:48:09 +0900

    test: 게임에서 1점 얻는 round win과 10점을 먼저 얻었을때의 테스트 코드 작성

--- backend/transcendence/games/test_game_consumer.py
+++ backend/transcendence/games/test_game_consumer.py
@@ -8,14 +8,18 @@ django.setup()
 
 import pytest
 from channels.testing import WebsocketCommunicator
-from .consumers import GameConsumer
+from .game_consumer import GameConsumer
+from .game_queue_consumer import GameQueueConsumer
 from channels.layers import get_channel_layer
 import json
 from members.models import Members
+from games.models import Game, Participant
 from rest_framework_simplejwt.tokens import RefreshToken
+from datetime import datetime
+from django.core.cache import cache
 
 @pytest.mark.asyncio
-async def test_game_consumer():
+async def test_round_win_success():
 
     channel_layer = get_channel_layer()
 
@@ -24,25 +28,87 @@ async def test_game_consumer():
     refresh = RefreshToken.for_user(fake_user)
     fake_token = str(refresh.access_token)
 
-    #토큰과 함께 /ws/game/<int:room_id> 에 연결
-    communicator = WebsocketCommunicator(GameConsumer.as_asgi(), "/ws/game/42?token=" + fake_token)
-    connected, subprotocol = await communicator.connect()
+    invite_user = Members.objects.create(nickname = 'invite_user', email = 'invite@test.com', is_2fa = False)
+    invite_refresh = RefreshToken.for_user(invite_user)
+    invite_token = str(invite_refresh.access_token)
 
-    assert connected
+    invite_time = datetime(2024, 2, 19, 12, 0, 0)
+    accept_time = datetime(2024, 2, 19, 12, 0, 42)
 
-    #GameConsumer의 receive에 json send
-    await communicator.send_json_to({
-        "type": "game_message",
-        "message": "Test Message"
+    iso_8601_accept_time = accept_time.isoformat()
+    iso_8601_invite_time = invite_time.isoformat()
+
+
+    #fake_user가 invite_user 초대
+    invite_communicator = WebsocketCommunicator(GameQueueConsumer.as_asgi(), "/ws/join_queue?token=" + fake_token)
+    invite_connected, invite_subprotocol = await invite_communicator.connect()
+
+    assert invite_connected
+
+    await invite_communicator.send_json_to({
+        "action": "invite_normal_queue",
+        "game_mode": Game.GameOption.CLASSIC,
+        "user_id": fake_user.id,
+        "invite_user_id": invite_user.id,
+        "invite_time": iso_8601_invite_time
     })
 
-    response = await communicator.receive_json_from()    
+    invite_response = await invite_communicator.receive_json_from()    
 
-    #GameConsumser에서 send한 값 테스트
-    assert response == {
-        'type': 'game_message',
-        'message': 'hello'
-    }
+    assert invite_response["status"] == "game create success"
 
-    await communicator.disconnect()
+    join_communicator = WebsocketCommunicator(GameQueueConsumer.as_asgi(), "/ws/join_queue?token=" + invite_token)
+    join_connected, invite_subprotocol = await join_communicator.connect()
+
+
+    assert join_connected
+
+    await join_communicator.send_json_to({
+        "action": "join_invite_normal_queue",
+        "game_id": invite_response["game_id"],
+        "accept_time": iso_8601_accept_time,
+        "user_id": invite_user.id
+    })
+
+    join_response = await join_communicator.receive_json_from()    
+
+    assert join_response["status"] == "game_start_soon"
+
+
+    await invite_communicator.disconnect()
+    await join_communicator.disconnect()
+
+
+    #게임방 입장
+    fake_communicator = WebsocketCommunicator(GameConsumer.as_asgi(), "/ws/game/" + str(invite_response["game_id"]) + "?token=" + fake_token)
+    fake_connected, fake_subprotocol = await fake_communicator.connect()
+
+    assert fake_connected
+
+
+    opponent_communicator = WebsocketCommunicator(GameConsumer.as_asgi(), "/ws/game/" + str(invite_response["game_id"]) + "?token=" + invite_token)
+    opponent_connected, opponent_subprotocol = await opponent_communicator.connect()    
+
+    assert opponent_connected
+
+    #게임 1번 이겼을 때 성공 테스트
+    await fake_communicator.send_json_to({
+        "action": "round_win",
+    })
+
+    round_win_response = await fake_communicator.receive_json_from()    
+
+    assert round_win_response["status"] == "round_over"
+
+    #게임 10번 이겼을 때 성공 테스트
+    for i in range(1, 10):
+        await fake_communicator.send_json_to({
+            "action": "round_win",
+        })
+        game_over_response= await fake_communicator.receive_json_from()
+
+    assert game_over_response["status"] == "normal_game_over"
+
+
+
          
