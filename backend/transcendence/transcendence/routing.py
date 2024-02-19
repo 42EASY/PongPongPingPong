e4a306b5fcc27745fb2c436e09156c030da6b9b7
dlwndms0812 <dlwndms0812@naver.com>
2024-02-19 14:11:55 +0900

    rename: 테스트용으로 작성한 consumer를 game을 진행하는 consumer로 사용하기 위해 이름 변경

--- backend/transcendence/transcendence/routing.py
+++ backend/transcendence/transcendence/routing.py
@@ -1,11 +1,11 @@
-from django.urls import re_path, path
-from games.consumers import GameConsumer
+from django.urls import re_path
+from games.game_consumer import GameConsumer
 from games.game_queue_consumer import GameQueueConsumer 
 from games.game_room_consumer import GameRoomConsumer
 from chat import consumers
 
 websocket_urlpatterns = [
-    path(r'ws/game/<int:room_id>', GameConsumer.as_asgi()), #TODO: 테스트용이므로 지울 예정
+    re_path(r'ws/game/(?P<game_id>\w+)/$', GameConsumer.as_asgi()),
     re_path(r'ws/join_queue/$', GameQueueConsumer.as_asgi()),
     re_path(r'ws/join/$', consumers.BaseConsumer.as_asgi()),
     re_path(r'ws/chat/(?P<target_user_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
