8fccbc1efbbc84c32d18f50d294d4bfce87ea0e0
정성은 <soc06241@naver.com>
2024-03-22 15:49:37 +0900

    fix: chatContents 오타 수정

--- frontend/src/pages/ChatRoom.js
+++ frontend/src/pages/ChatRoom.js
@@ -6,7 +6,7 @@ import { getUserInfo } from "../components/Main/UserApi.js";
 import { getUserId } from "../state/State.js";
 import { addChatContent } from "../state/ChatState.js";
 
-function sendMessage(id, me) {
+async function sendMessage(id, me) {
   const $chatInput = document.querySelector(".chatInput");
   const $chatContents = document.querySelector("#chatContents");
   const data = {
@@ -31,7 +31,7 @@ export default async function ChatRoom(id) {
 
   //채팅 내용
   const $chatContents = ChatContents(user.result, me.result);
-  $chatContents.id = "chatContetns";
+  $chatContents.id = "chatContents";
   $chatsWrapper.appendChild($chatContents);
 
   //채팅 입력칸
