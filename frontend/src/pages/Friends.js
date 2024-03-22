8c5018ac54b6089100c2f0a58670f4d63721ec9f
정성은 <soc06241@naver.com>
2024-03-22 16:08:59 +0900

    fix: friends 목록 수정

--- frontend/src/pages/Friends.js
+++ frontend/src/pages/Friends.js
@@ -6,47 +6,46 @@ import { getFriends, getBlockeds } from "../components/Friends/ListApi.js";
 
 let isFriends = true;
 let curPage = 1;
-let dataSize = 8;
+let dataSize = 10;
 let isFetching = false;
 let hasMore = true;
 
 async function fetchFriends(keyword) {
-  const $wrapper = document.querySelector(".listWrapper");
-
   isFriends = true;
   isFetching = true;
   const flist = await getFriends(keyword, curPage, dataSize);
-  console.log(flist);
   isFetching = false;
 
-  const list = FriendList(flist.result.data);
-  if (flist.result.data.length === 0 || flist.result.data.length < dataSize) {
+  if (
+    flist.result.data.length === 0 ||
+    flist.result.data.length < dataSize ||
+    curPage === flist.result.total_page
+  ) {
     hasMore = false;
-    if (list.innerHTML !== $wrapper.innerHTML) $wrapper.innerHTML = "";
   } else {
     curPage++;
   }
-  $wrapper.appendChild(list);
+  return FriendList(flist.result.data);
 }
 
 async function fetchBlockeds(keyword) {
-  const $wrapper = document.querySelector(".listWrapper");
-
   isFriends = false;
   isFetching = true;
   const blist = await getBlockeds(keyword, curPage, dataSize);
   isFetching = false;
-  const list = BlockedList(blist.result.data);
-  if (blist.result.data.length === 0 || blist.result.data.length < dataSize) {
+  if (
+    blist.result.data.length === 0 ||
+    blist.result.data.length < dataSize ||
+    curPage === blist.result.total_page
+  ) {
     hasMore = false;
-    if (list.innerHTML !== $wrapper.innerHTML) $wrapper.innerHTML = "";
   } else {
     curPage++;
   }
-  $wrapper.appendChild(list);
+  return BlockedList(blist.result.data);
 }
 
-export default function Friends() {
+export default async function Friends() {
   const $sidebar = document.querySelector(".sidebar");
   $sidebar.innerHTML = "";
 
@@ -68,7 +67,7 @@ export default function Friends() {
   $listWrapper.classList.add("listWrapper");
   $friendsWrapper.appendChild($listWrapper);
 
-  fetchFriends("");
+  $listWrapper.appendChild(await fetchFriends(""));
 
   //사이드바 외부 영역
   const $overlay = document.createElement("div");
@@ -78,6 +77,7 @@ export default function Friends() {
   $overlay.addEventListener("click", (e) => {
     $friendsWrapper.classList.remove("showSidebar");
     $overlay.classList.remove("showOverlay");
+    curPage = 1;
   });
   $sidebar.appendChild($overlay);
 
@@ -88,25 +88,27 @@ export default function Friends() {
     curPage = 1;
     hasMore = true;
     $searchInput.value = "";
+    $listWrapper.innerHTML = "";
     if (e.target.classList.contains("friendList")) {
       $friendList.classList.add("friendsTitleSelect", "titleSelect");
       $blockedList.classList.remove("friendsTitleSelect", "titleSelect");
-      fetchFriends("");
+      $listWrapper.appendChild(await fetchFriends(""));
     } else if (e.target.classList.contains("blockedList")) {
       $friendList.classList.remove("friendsTitleSelect", "titleSelect");
       $blockedList.classList.add("friendsTitleSelect", "titleSelect");
-      fetchBlockeds("");
+      $listWrapper.appendChild(await fetchBlockeds(""));
     }
   });
 
   //검색 입력 이벤트
   const $searchInput = document.getElementById("searchInput");
-  $searchInput.addEventListener("input", () => {
-    console.log($searchInput.value);
+  $searchInput.addEventListener("keyup", async () => {
     curPage = 1;
     hasMore = true;
-    if (isFriends) fetchFriends($searchInput.value);
-    else fetchBlockeds($searchInput.value);
+    $listWrapper.innerHTML = "";
+    if (isFriends)
+      $listWrapper.appendChild(await fetchFriends($searchInput.value));
+    else $listWrapper.appendChild(await fetchBlockeds($searchInput.value));
   });
 
   //todo: scroll event
