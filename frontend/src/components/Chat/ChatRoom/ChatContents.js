c38a8b2078ef46a10540ff5da0a3a419093d75d7
정성은 <soc06241@naver.com>
2024-03-27 14:17:40 +0900

    style: chatContentsWrapper 클래스 삭제

--- frontend/src/components/Chat/ChatRoom/ChatContents.js
+++ frontend/src/components/Chat/ChatRoom/ChatContents.js
@@ -2,7 +2,6 @@ import ChatContent from "./ChatContent.js";
 
 export default async function ChatContents(messages) {
   const $chatContentsWrapper = document.createElement("div");
-  $chatContentsWrapper.classList.add("chatContentsWrapper");
 
   for (const message of messages) {
     const $chat = ChatContent(message.sender, message);
