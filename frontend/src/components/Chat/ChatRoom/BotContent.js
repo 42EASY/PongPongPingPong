79a5f24494e9963c28885374bd1be8141da37371
정성은 <soc06241@naver.com>
2024-03-27 18:53:18 +0900

    feat: bot message 추가

---
+++ frontend/src/components/Chat/ChatRoom/BotContent.js
@@ -0,0 +1,103 @@
+import { getNickname } from "../../../state/State.js";
+import Modal from "../../Modal/Modal.js";
+import {
+  joinInviteNormalQueue,
+  joinInviteTournamentQueue,
+} from "../../Nav/JoinQueue.js";
+import { formatTimestamp } from "./ChatContent.js";
+
+export default function BotContent(data) {
+  const $botContentWrapper = document.createElement("div");
+  $botContentWrapper.classList.add("chatContentWrapper");
+
+  const $botContentImg = document.createElement("img");
+  $botContentImg.setAttribute("src", "./src/images/sponge.png");
+  $botContentImg.setAttribute("alt", "botProfileImg");
+  $botContentImg.classList.add("chatContentImg");
+
+  const $botContentRight = document.createElement("div");
+  $botContentRight.classList.add("chatContentRight");
+
+  const $botContentInfo = document.createElement("div");
+  $botContentInfo.classList.add("chatContentInfo");
+
+  const $botContentName = document.createElement("div");
+  $botContentName.classList.add("chatContentName");
+  $botContentName.innerHTML = "announcement_bot";
+
+  const $botContentTime = document.createElement("div");
+  $botContentTime.classList.add("chatContentTime");
+  $botContentTime.innerHTML = formatTimestamp();
+
+  const $botContentBox = document.createElement("div");
+
+  const $botContent = document.createElement("div");
+  $botContent.classList.add("chatChatContent");
+
+  const $botContentBtn = document.createElement("div");
+  $botContentBtn.classList.add("btn", "chatContentBtn");
+
+  $botContentInfo.appendChild($botContentName);
+  $botContentInfo.appendChild($botContentTime);
+  $botContentRight.appendChild($botContentInfo);
+  $botContentRight.appendChild($botContentBox);
+  $botContentBox.appendChild($botContent);
+  $botContentWrapper.appendChild($botContentImg);
+  $botContentWrapper.appendChild($botContentRight);
+
+  if (data.action === "bot_notify_tournament_game_result") {
+    const msg = `토너먼트가 종료되었습니다`;
+    $botContent.innerHTML = msg;
+    $botContentBtn.innerHTML = "결과 확인하기";
+    $botContentBox.appendChild($botContentBtn);
+
+    $botContentBtn.onclick = () => {
+      // todo: gameResultTabe 모달 띄우기
+    };
+  }
+  if (data.action === "bot_notify_invited_normal_game") {
+    const mode = data.mode === "SPEED" ? "스피드" : "클래식";
+    const msg = `${data.inviter.nickname} 님이 일반 게임 - ${mode} 게임을 요청했습니다`;
+    $botContent.innerHTML = msg;
+    $botContentBtn.innerHTML = "게임하러 가기";
+    $botContentBox.appendChild($botContentBtn);
+
+    $botContentBtn.onclick = () => {
+      if (
+        !joinInviteNormalQueue({
+          action: "join_invite_normal_queue",
+          game_id: data.game_id,
+        })
+      ) {
+        Modal("invalidGame");
+        $botContentBtn.classList.add("invalidBtn");
+      }
+    };
+  }
+  if (data.action === "bot_notify_invited_tournament_game") {
+    const msg = `${data.inviter.nickname} 님이 토너먼트 게임을 요청했습니다`;
+    $botContent.innerHTML = msg;
+    $botContentBtn.innerHTML = "게임하러 가기";
+    $botContentBox.appendChild($botContentBtn);
+
+    $botContentBtn.onclick = () => {
+      if (
+        !joinInviteTournamentQueue({
+          action: "join_invite_tournament_queue",
+          room_id: data.game_id,
+        })
+      ) {
+        Modal("invalidGame");
+        $botContentBtn.classList.add("invalidBtn");
+      }
+    };
+  }
+  if (data.action === "bot_notify_tournament_game_opponent") {
+    const msg = `${getNickname()} 님의 다음 대결 상대는 ${
+      data.opponent.nickname
+    } 님 입니다`;
+    $botContent.innerHTML = msg;
+  }
+
+  return $botContentWrapper;
+}
