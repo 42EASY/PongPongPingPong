e4a7af2049c577ae28c5ac5267dc69ef70fd95aa
정성은 <soc06241@naver.com>
2024-03-27 18:50:51 +0900

    fix: formatTimestamp() 클라이언트 시간으로 변경

--- frontend/src/components/Chat/ChatRoom/ChatContent.js
+++ frontend/src/components/Chat/ChatRoom/ChatContent.js
@@ -1,13 +1,13 @@
 import Profile from "../../../pages/Profile.js";
 import { getUserId } from "../../../state/State.js";
 
-function formatTimestamp(timestamp) {
-  const date = new Date(timestamp);
+export function formatTimestamp() {
+  const date = new Date();
   const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
   return `${year}-${month}-${day} ${hours}:${minutes}`;
 }
 
@@ -32,7 +32,7 @@ export default function ChatContent(user, data) {
 
   const $chatContentTime = document.createElement("div");
   $chatContentTime.classList.add("chatContentTime");
-  $chatContentTime.innerText = formatTimestamp(data.timestamp);
+  $chatContentTime.innerText = formatTimestamp();
 
   const $chatContent = document.createElement("div");
   $chatContent.classList.add("chatContent");
