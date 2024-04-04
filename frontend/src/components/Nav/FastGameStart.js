d8b3c2e61ef3d059297dfc0ceeeafed19c5d984c
정성은 <soc06241@naver.com>
2024-04-04 20:39:23 +0900

    fix: game option, mode 바꿔서 작성한 부분 수정

--- frontend/src/components/Nav/FastGameStart.js
+++ frontend/src/components/Nav/FastGameStart.js
@@ -7,16 +7,16 @@ import {
 } from "./JoinQueue.js";
 
 export default function FastGameStart() {
-  let game_mode;
+  let game_option;
   Modal("gameMode").then((result) => {
     if (result.isPositive && result.input === "2P 게임") {
       Modal("gameOption").then((option) => {
         if (option.isPositive) {
-          if (option.input === "클래식") game_mode = "CLASSIC";
-          else if (option.input === "스피드") game_mode = "SPEED";
+          if (option.input === "클래식") game_option = "CLASSIC";
+          else if (option.input === "스피드") game_option = "SPEED";
           changeUrl("/game", {
-            option: "2P",
-            mode: game_mode,
+            mode: "2p",
+            option: game_option,
           });
         }
       });
@@ -24,15 +24,14 @@ export default function FastGameStart() {
     if (result.isPositive && result.input === "일반 게임") {
       Modal("gameOption").then((option) => {
         if (option.isPositive) {
-          if (option.input === "클래식") game_mode = "CLASSIC";
-          else if (option.input === "스피드") game_mode = "SPEED";
+          if (option.input === "클래식") game_option = "CLASSIC";
+          else if (option.input === "스피드") game_option = "SPEED";
           joinNormalQueue({
             action: "join_normal_queue",
-            game_mode: game_mode,
+            game_mode: game_option,
           });
           Modal("waitingPlayer").then((res) => {
-            // 모달 취소하기 버튼 눌렀을 경우
-            if (!res.isPositive) cancelNormalQueue({ action: "" });
+            if (!res.isPositive) cancelNormalQueue({ action: "cancel_queue" });
           });
         }
       });
