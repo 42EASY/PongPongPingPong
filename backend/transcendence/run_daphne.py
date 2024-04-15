4a7035c1389f67151ba8912d1ce899348b7b6da4
eunbi9n <gombi9_9@naver.com>
2024-04-16 01:13:38 +0900

    fix: 서버에서 mkcert를 사용하도록 변경

--- backend/transcendence/run_daphne.py
+++ backend/transcendence/run_daphne.py
@@ -4,7 +4,7 @@ from subprocess import Popen
 import os
 
 def runserver():
-    cmd = ['daphne', '-e', 'ssl:8443:privateKey=/app/keys/private/localhost-key.pem:certKey=/app/keys/certs/localhost.pem', 'transcendence.asgi:application']
+    cmd = ['daphne', '-e', 'ssl:8443:privateKey=/etc/ssl/private/localhost.key:certKey=/etc/ssl/certs/localhost.pem', 'transcendence.asgi:application']
     process = Popen(cmd)
     try:
         for changes in watch('/app/transcendence'):
