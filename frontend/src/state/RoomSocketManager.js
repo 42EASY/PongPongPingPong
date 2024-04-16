53233de99a837d8aa4f8b6cb4cf05a7a124e8949
정성은 <soc06241@naver.com>
2024-04-16 20:51:01 +0900

    fix: roomsocket disconnect 되도록 수정

--- frontend/src/state/RoomSocketManager.js
+++ frontend/src/state/RoomSocketManager.js
@@ -3,15 +3,31 @@ import { getAccessToken, socketBaseUrl } from "./State.js";
 const RoomSocketManager = (() => {
   let instance;
   let roomNumber;
+  let reconnectInterval = 1000; // 재연결 시도 간격 초기값
+  let maxReconnectAttempts = 10; // 최대 재연결 시도 횟수
+  let reconnectAttempts = 0; // 현재 재연결 시도 횟수
 
   function init(room_id) {
+    console.log("init: ", room_id);
     roomNumber = room_id;
     const socketUrl = `${socketBaseUrl}/ws/join_room/${room_id}/?token=${getAccessToken()}`;
     const ws = new WebSocket(socketUrl);
 
     ws.onclose = (e) => {
-      if (e.wasClean) console.log("[gameroom - close] - normal");
-      else console.log("[gameroom - close] - abnormal");
+      if (e.wasClean) console.log("[roomgame - close] - normal");
+      else {
+        console.log("[roomgame - close] - abnormal");
+        console.log("roomWebSocket 연결 끊김, 재연결 시도:", e);
+        if (reconnectAttempts < maxReconnectAttempts) {
+          setTimeout(function () {
+            instance = init(); // 재연결 시도
+          }, reconnectInterval);
+          reconnectInterval *= 2; // 재연결 시도 간격 증가
+          reconnectAttempts++;
+        } else {
+          console.log("최대 재연결 시도 횟수 도달");
+        }
+      }
     };
 
     ws.onerror = (e) => {
@@ -38,7 +54,7 @@ const RoomSocketManager = (() => {
   return {
     getInstance: function (room_id) {
       if (room_id === undefined) return instance;
-      if (!instance) {
+      if (!instance || instance.readyState === WebSocket.CLOSED) {
         instance = init(room_id);
       } else if (roomNumber !== room_id) {
         instance.close();
