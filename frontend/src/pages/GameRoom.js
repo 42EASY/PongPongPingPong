5c3c3f2e8a7c2a80f0c53f8c19ab1237e5d71ea7
정성은 <soc06241@naver.com>
2024-04-17 14:23:50 +0900

    style: gameroom - final 안내문구로 변경

--- frontend/src/pages/GameRoom.js
+++ frontend/src/pages/GameRoom.js
@@ -3,6 +3,7 @@ import WaitingPlayer from "../components/GameRoom/WaitingPlayer.js";
 import changeUrl from "../Router.js";
 import Modal from "../components/Modal/Modal.js";
 import RoomSocketManager from "../state/RoomSocketManager.js";
+import TimerRing from "../components/GameRoom/TimerRing.js";
 
 export default function GameRoom(data) {
   document.body.style.display = "block";
@@ -26,12 +27,15 @@ export default function GameRoom(data) {
   const waitingPlayersArr = [];
   if (data.round === "FINAL") {
     $waitingPlayers.id = "waitingFinalPlayers";
-    playerLength = 2;
-    for (let i = 0; i < playerLength; i++) {
-      waitingPlayersArr[i] = document.createElement("div");
-      $waitingPlayers.appendChild(waitingPlayersArr[i]);
-    }
-    waitingPlayersArr[0].id = "waitingPlayer2";
+    const textbox1 = document.createElement("div");
+    const textbox2 = document.createElement("div");
+    textbox1.classList.add("waitingText");
+    textbox2.classList.add("waitingText");
+    textbox1.innerHTML = "상대방이 아직 게임 중입니다";
+    textbox2.innerHTML = "🧽 잠시만 기다려 주세요 🧽";
+    $waitingPlayers.append(textbox1);
+    $waitingPlayers.append(textbox2);
+    $waitingPlayers.appendChild(TimerRing());
   } else {
     for (let i = 0; i < playerLength; i++) {
       waitingPlayersArr[i] = document.createElement("div");
@@ -75,7 +79,7 @@ export default function GameRoom(data) {
         changeUrl("/game", res);
       });
     }
-    if (res.status === "player_entrance") {
+    if (res.status === "player_entrance" && data.round === "SEMI_FINAL") {
       let i;
       for (i = 0; i < res.player_info.length; i++) {
         waitingPlayersArr[i].innerHTML = "";
